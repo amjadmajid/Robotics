985dd7a11f979c406c1c88a6bcc156697bfaaced
amjadmajid <amjad.y.majid@gmail.com>
2022-02-22 12:21:48 +0100

    filter the speed of a dc motor

--- 03_filtering_motor_speed/03_filtering_motor_speed.ino
+++ 03_filtering_motor_speed/03_filtering_motor_speed.ino
@@ -15,10 +15,17 @@
   // can be changed by the hardware (i.e., by ISR)
   volatile unsigned long ticks_cntr = 0;
 
-  float DEBUG_delta_ticks = 0;
-  
+  float prev_delta_ticks1 = 0;
+  float prev_delta_ticks2 = 0;
+
+  // Low pass filter
+  float rpmFilt = 0.0;
+  float prevRpmFilt = 0.0;
+  float prevRpm = 0.0;
+  int idx = 0;
   void setup(){
-    Serial.begin(9600);
+    Serial.begin(115200);
+    Serial.println("Filtered:, Raw:");
     // set GPIO pin modes 
     pinMode(encA, INPUT_PULLUP);
     pinMode(dir1, OUTPUT);
@@ -41,26 +48,37 @@
   void encA_ISR(){ ticks_cntr += 1; }
 
 float rotation_count(){
-    float delta_ticks   = (float) (ticks_cntr - prev_ticks_cntr);
-    DEBUG_delta_ticks = delta_ticks;
+    float delta_ticks   = ((float) (ticks_cntr - prev_ticks_cntr) + 
+    prev_delta_ticks1 + prev_delta_ticks2) / 3.0;
+    prev_delta_ticks2 = prev_delta_ticks1;
+    prev_delta_ticks1 = delta_ticks;
     prev_ticks_cntr     = ticks_cntr;
     return delta_ticks / TICKS_PER_REVOLUTION;
   }
 
   void loop(){
-    float time_dif = (float) (millis() - prev_time) ;
+    float time_dif = (float) (micros() - prev_time) ;
     
-    if (time_dif >= 10){ 
+    if (time_dif >= 10000){ 
       float  rotations  = rotation_count();
       prev_time += time_dif;  
-      float minutes  = time_dif/ 60.0e3;
+      float minutes  = time_dif/ 60.0e6;
       float rpm = rotations / minutes; 
 
+       // Low pass filter
+      float rpmFilt = .521 * prevRpmFilt + 0.239 * rpm + 0.239 * prevRpm;
+      prevRpmFilt = rpmFilt;
+      prevRpm = rpm;
+
       // output to the screen through serial port
-//      Serial.print(DEBUG_delta_ticks);
-//      Serial.print(" ");
-      Serial.print(rpm);
-      Serial.println();
+      idx +=1;
+      if (idx >= 3) {
+        Serial.print(rpmFilt);
+        Serial.print(" ");
+        Serial.print(rpm);
+        Serial.println();
+        idx=0;
+      }
     }
   }
 
